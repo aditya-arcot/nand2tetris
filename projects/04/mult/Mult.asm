9e398a6e1cc19f6adb0981b26e2758dc3395434c
aditya-arcot <arcot.aditya@utexas.edu>
2023-02-04 02:53:05 -0600

    finished Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -4,9 +4,49 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
+
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+  // a = R0
+  @R0
+  D=M
+  @a
+  M=D
+
+  // b = R1
+  @R1
+  D=M
+  @b
+  M=D
+
+  // R2 = 0
+  @R2
+  M=0
+
+
+(LOOP)
+  // if b == 0 jump to END
+  @b
+  D=M
+  @END
+  D;JEQ
+
+  // b -= 1
+  @b
+  M=M-1
+
+  // R2 += a
+  @a
+  D=M
+  @R2
+  M=M+D
+
+  // restart LOOP
+  @LOOP
+  0;JMP
+
+
+(END)
+  @END
+  0;JMP
