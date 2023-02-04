d93d584cedbd647620dcd0ef252ee8227204264a
aditya-arcot <arcot.aditya@utexas.edu>
2023-02-04 04:31:03 -0600

    finished project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,66 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+// infinite loop
+(LISTEN)
+  // reset RAM[addr] to SCREEN
+  @SCREEN
+  D=A
+  @addr
+  M=D
+
+  // reset color to -1
+  @color
+  M=-1
+
+  // read RAM[KBD]
+  @KBD
+  D=M
+
+  // if greater than 0, jump to BLACK
+  @BLACK
+  D;JGT
+
+(WHITE)
+  @color
+  M=0
+
+  @FILL
+  0;JMP
+
+(BLACK)
+  // no extra code needed
+  // continues to FILL
+
+(FILL)
+  // check diff between RAM[addr] and KBD
+  // if 0, jump to LISTEN
+  @addr
+  D=M
+  @KBD
+  D=A-D
+  @LISTEN
+  D;JEQ
+
+  // get color
+  @color
+  D=M
+
+  // set row color
+  @addr
+  A=M
+  M=D
+
+  // addr += 1
+  @addr
+  M=M+1
+
+  // restart FILL
+  @FILL
+  0;JMP
